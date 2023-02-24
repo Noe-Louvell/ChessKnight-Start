1f0e33864909933dd7c13a80d2084c113ccc8d50
Noé <60090776+Noe-Louvell@users.noreply.github.com>
2023-02-24 12:33:41 +0100

    Update ChessKnightProblem.java

--- src/com/jad/chessknight/ChessKnightProblem.java
+++ src/com/jad/chessknight/ChessKnightProblem.java
@@ -1,24 +1,43 @@
 package com.jad.chessknight;
 
-/**
- * The type Chess knight problem.
- */
 public abstract class ChessKnightProblem {
-    /**
-     * Solve.
-     *
-     * @throws CloneNotSupportedException the clone not supported exception
-     */
     public static void solve() throws CloneNotSupportedException {
         ChessBoard chessBoard = new ChessBoard();
-        ChessKnight chessKnight = new ChessKnight(chessBoard,0,0);
+        ChessKnight chessKnight = new ChessKnight(chessBoard, 0, 0);
 
-        for (int y = 0; y < chessBoard.getHeight(); y++) {
-            for (int x = 0; x < chessBoard.getWidth(); x++) {
-                chessKnight.moveTo(x, y);
+        int xMove[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int yMove[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        chessBoard.saveStepAt(0, 0, 0);
+        if (!solveKTUtil(0, 0, 1, xMove, yMove, chessKnight, chessBoard)) {
+            System.out.println("Solution does not exist");
+        } else
+            System.out.println(chessBoard);
+    }
+
+    static boolean isSafe(int x, int y, ChessBoard chessBoard) {
+        return (x >= 0 && x < 8 && y >= 0 && y < 8
+                && chessBoard.getAt(x, y).getValue() == -1);
+    }
+
+    static boolean solveKTUtil(int x, int y, int movei, int xMove[], int yMove[], ChessKnight chessKnight,
+            ChessBoard chessBoard) {
+        int k, next_x, next_y;
+        if (movei == 8 * 8)
+            return true;
+
+        for (k = 0; k < 8; k++) {
+            next_x = x + xMove[k];
+            next_y = y + yMove[k];
+            if (isSafe(next_x, next_y, chessBoard)) {
+                chessBoard.saveStepAt(next_x, next_y, movei);
+                if (solveKTUtil(next_x, next_y, movei + 1, xMove, yMove, chessKnight, chessBoard))
+                    return true;
+                else
+                    chessBoard.saveStepAt(next_x, next_y, -1);
             }
         }
 
-        System.out.println(chessBoard);
+        return false;
     }
+
 }
